5e6116d56a2dc8db85d7b07749bd00e9499c2e4a
Mamon <cszetsu@gmail.com>
2018-03-26 19:44:02 +0800

    ~patientManagement(integ)

--- INTEGRATION/H-M-S/controllers/doctorRoute.js
+++ INTEGRATION/H-M-S/controllers/doctorRoute.js
@@ -198,11 +198,11 @@ var user, Aid, availableBedss, p;
           if(req.query.patient){
             var sql  = "SELECT patient_id,patient_type,name,age,sex,blood_type FROM patient where patient_id = "+req.query.patient+";";
             db.query(sql, function(err, rows){
-              res.render('doctor/patientManagement', {p:rows, p2:null, username:user, invalid:null});
+              res.render('doctor/patientManagement', {p:rows, p2:null, med:null, username:user, invalid:null});
             });
           } else {
               db.query(patientManagementSQL, function(err, rows){
-                res.render('doctor/patientManagement', {p:rows, p2:null, username:user, invalid:null});
+                res.render('doctor/patientManagement', {p:rows, p2:null, med:null, username:user, invalid:null});
               });
           }
         } else {
@@ -223,19 +223,14 @@ var user, Aid, availableBedss, p;
             } else if(isMatch) {
               var sql  = "SELECT patient_id,patient_type,name,age,sex,blood_type FROM patient where patient_id = "+req.query.passPatient+";";
               var sql2  = "SELECT * FROM patient where patient_id = "+req.query.passPatient+";";
-              var latestMedicine =  'SELECT medicine FROM `prescription` where status="confirmed" and patient_id = '+req.query.passPatient+' and creation_stamp = (SELECT creation_stamp from prescription where patient_id = '+req.query.passPatient+' and status="confirmed" order by creation_stamp desc limit 1);'
-              db.query(sql + sql2 + latestMedicine, function(err, successRows){
-                var medicineParse = JSON.parse(JSON.stringify(successRows[2]));
-                var medicine= '';
-                for (var i = 0; i < medicineParse.length; i++) {
-                  medicine += medicineParse[i].medicine + ',\n';
-                }
-                res.render('doctor/patientManagement', {p:successRows[0], p2:successRows[1], medicine:medicine, username:user, invalid:null});
+              var med = "select date_stamp, lab, medicine,diagnosis,bed from patient_history where patient_id = "+req.query.passPatient+" order by date_stamp;";
+              db.query(sql + sql2 + med, function(err, successRows){
+                res.render('doctor/patientManagement', {p:successRows[0], p2:successRows[1], med:successRows[2], username:user, invalid:null});
               });
             } else {
               var sql  = "SELECT patient_id,patient_type,name,age,sex,blood_type FROM patient where patient_id = "+req.query.passPatient+";";
               db.query(sql, function(err, errorRows){
-                res.render('doctor/patientManagement', {p:errorRows, p2:null, username:user, invalid:'error'});
+                res.render('doctor/patientManagement', {p:errorRows, p2:null, med:null, username:user, invalid:'error'});
               });
             }
           });
