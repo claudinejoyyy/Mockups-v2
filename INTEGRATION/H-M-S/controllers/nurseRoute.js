e90d7c4644b95ea21af3924334f3a5095dcfa6b1
jen0123 <2152195@slu.edu.ph>
2018-03-26 18:08:54 +0800

    removed limit 5

--- INTEGRATION/H-M-S/controllers/nurseRoute.js
+++ INTEGRATION/H-M-S/controllers/nurseRoute.js
@@ -244,7 +244,7 @@ var fhSQL       = "SELECT name FROM family_history;";
     if(req.session.email && req.session.sino == 'nurse'){
       if (req.session.sino == 'nurse') {
         var profileInfoSQL  = 'SELECT * from user_accounts where account_id = '+req.session.Aid+';';
-        var activityLogsSQL = 'SELECT * from activity_logs where account_id = '+req.session.Aid+' ORDER by logs_id desc LIMIT 5;';
+        var activityLogsSQL = 'SELECT * from activity_logs where account_id = '+req.session.Aid+' ORDER by logs_id desc;';
         db.query(profileInfoSQL + activityLogsSQL, function(err, rows){
           if (err) {
             console.log(err);
